8f20ffa816c01167f606f170a783f7ea19694d40
Rand <randalobaid9@gmail.com>
2024-12-18 21:49:13 +0300

    Initial Commit

---
+++ BayanApp/BayanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BayanAppApp.swift
+//  BayanApp
+//
+//  Created by Rand Soliman Alobaid on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BayanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
