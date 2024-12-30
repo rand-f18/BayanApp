c46cc020f8e4bda639eec427bb4f6452cb53135f
wafa14161995 <Wafa14161995@gmail.com>
2024-12-30 16:57:46 +0300

    assest chnges

--- BayanApp/LettersPageModel.swift
+++ BayanApp/LettersPageModel.swift
@@ -36,7 +36,7 @@ class LettersViewModel: ObservableObject {
                 sound: "Kletter",
                 descLearning: "يلتقي وسط اللسان بسقف الحنك التقاء محكماً بحيث ينحبس مجرى الهواء\nوإذا انفصل العضوان انفصالاً بطيئاً يسمع صوت الجيم",
                 descrTraining: "١-يضع الطفل يده اليمنى تحت الذقن للشد على الحنك\n٢-و يضع يده الاخرى على حنجرته وذلك لكي يشعر بالاهتزازات الناتجة عند إخراج الصوت",
-                ImageProunounce: "G",
+                ImageProunounce: "Gem",
                 buttonImage: "camelButton"
             ),
             LettterModel(
@@ -54,7 +54,7 @@ class LettersViewModel: ObservableObject {
                 sound: "Rletter",
                 descLearning: "يتكون صوت الراء بواسطة التقاء طرف اللسان باللثة\nويبتعد عنها مرات عديدة فيسمع الصوت على صورة مجموعة من الانسحابات والانفجارات",
                 descrTraining: "١-استخدم قابض اللسان لوضعه فوق اﻷسنان لوضعه فوق الاسنان\nالامامية السفلية تخت الاسنان كمساعد لإخراج الصوت مع وضع مرآة امام الطفل",
-                ImageProunounce: "R",
+                ImageProunounce: "Ra",
                 buttonImage: "RacconButton"
             ),
             LettterModel(
@@ -63,7 +63,7 @@ class LettersViewModel: ObservableObject {
                 sound: "Sletter",
                 descLearning: "يتكون صوت السين بتطابق الأسنان الامامية العليا على السفلى نسبياً\nووضع طرف اللسان خلف الأسنان العليا السفلى",
                 descrTraining: "١- يضع الطفل يده أمام فمه لكي يشع بالهواء البارد الخارج من فمه عند إنتاج الصوت\nيجب ملاحظة الفم بحيث يكون مبتسماً عند إنتاج الصوت(استخدم المرآة لملاحظة ذلك) ",
-                ImageProunounce: "S",
+                ImageProunounce: "Sa",
                 buttonImage: "TurtleButton"
             ),
             LettterModel(
@@ -72,7 +72,7 @@ class LettersViewModel: ObservableObject {
                 sound: "ShLetter",
                 descLearning: "يتكون صوت الشين بالتصاق طرف ووسط اللسان بالجزء العلوي الأمامي\nويصدر الصوت بشكل مشوشاً",
                 descrTraining: "١- يضع الطفل يده أمام فمه لكي يحس بالهواء الساخن عن إنتاج الصوت\n٢-يجب ملاحظة وضع الفم بحيث يكون مضموماً قليلاً عند إنتاج الصوت أكثر من صوت السين (استخدم المرآة لملاحظة ذلك)",
-                ImageProunounce: "Sh",
+                ImageProunounce: "Shen",
                 buttonImage: "LionButton"
             ),
             LettterModel(
@@ -81,7 +81,7 @@ class LettersViewModel: ObservableObject {
                 sound: "GhLetter",
                 descLearning: "تخرج الغين من أدنى الحلق مع منطقة الحنك اللحمي\nوقبل اللهاة مباشرة مع أصل اللسان",
                 descrTraining: "١-اطلب من الطفل وضع كمية من الماء في فمه ويطلب منه تحريكها عندة مرات على ان تبقى محجوزة في فمه (يقوم بالغرغره)\n٢-لكي يشعر الطفل بالصوت أيضاً فعليه أن يضع يده على حنجرته أثناء إنتاج الغين",
-                ImageProunounce: "Gh",
+                ImageProunounce: "Gen",
                 buttonImage: "GazelleButton"
             ),
             LettterModel(
@@ -90,7 +90,7 @@ class LettersViewModel: ObservableObject {
                 sound: "Kletter",
                 descLearning: "jhougi;ug;i",
                 descrTraining: "١-يرتفع أقصى اللسان حتى يلتقي بأاقصى الحنك الأعلى مع ضغط\n٢-اندفاع الهواء مع انخفاض اللسان محدثا عند اندفاعه صوتا انفجاريا وهو صوت الكاف",
-                ImageProunounce: "K",
+                ImageProunounce: "Kaf",
                 buttonImage: "kangrooButton"
             )
         ]
